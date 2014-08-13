52373d00e4bc17d0196c9fd2f4ad4cdc1bf469de
PaulWowk <paul.wowk@fishnetsecurity.com>
2014-08-13 17:54:16 -0600

    Updated to partially work

--- AutoCompleteBlog/Scripts/DetectPassManager.js
+++ AutoCompleteBlog/Scripts/DetectPassManager.js
@@ -1,40 +1,24 @@
-﻿var wrotepass = "false"; // Flag to show if the password has changed.
-var initialpw; // Set the first time updateWrotePass is called
-$(document).ready(function (event) {
-    $('#loginform').submit(function (ev) {
-        ev.preventDefault(); // to stop the form from submitting
+﻿
+// Put the form in the spot
+function submitForm() {
 
-        // Check if the password has changed
-        if (wrotepass == "true") {
-            // Submit the form
-            $('#loginform').attr('action', 'login');
-            $('#loginform').append("<input type=\"hidden\" name=\"usedPassManager\" value=\"false\">");
-            this.submit();
-        }
-            // Password has not changed
-        else {
-            // Record a new input to submit to the server
-            $('#loginform').append("<input type=\"hidden\" name=\"warningShown\" value=\"true\">");
-            // Show the warning message
-            alert("Do not use a password manager to save your password. Type it in manually and remove it" +
-                    " from your password manager.");
-        }
+    // Obtains the values fomr the input boxes 
+    var username = $("#UserName").val();
+    var password = $("#Password").val();
+
+    // This forms the post data into the correct format. Notice that it is URI encoded
+    var postdata = "UserName=" + encodeURIComponent(username) + "&Password=" + encodeURIComponent(password);
+
+    // This is the asynchronous message being sent along with the content.
+    $.ajax({
+        type: "POST",
+        url: "/Account/Login",
+        data: postdata
+    }).done(function (msg) {
+        // Authentication Success, this would most likely be a redirect, here it just 
+        // Fills out the response which contains the authenticated information.
+        document.open();
+        document.write(msg);
+        document.close();
     });
-});
-
-// Function that detects if the password was changed.
-// This function is called on a keypress event on the password input field.
-function updateWrotePass() {
-    // Check if the initialpw has been defined, if not, define initialpw
-    if ((typeof (initialpw) === "undefined")) {
-        initialpw = $('#Password').val();
-    }
-
-    // If the password has not been changed, check to see if it has changed
-    if (wrotepass === 'false') {
-        if (!(initialpw === $('#Password').val())) {
-            // The password has changed, update the flag to true
-            wrotepass = 'true';
-        }
-    }
 }
